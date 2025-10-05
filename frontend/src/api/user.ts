8c118c5d47514d44f935d186397722027ab0e806
mohayyad-muawia <mohayyad2.0@gmail.com>
2025-10-05 15:32:37 +0200

    updates

--- frontend/src/api/user.ts
+++ frontend/src/api/user.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_URL =
-  (import.meta.env.VITE_API_URL || "http://localhost:3000") + "/user";
+  (import.meta.env.VITE_API_URL || "http://10.109.195.216:3000") + "/user";
 const api = axios.create({ baseURL: API_URL });
 
 api.interceptors.request.use(
