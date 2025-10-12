0121e696e52dee34ea74b1e7a2e9c24dbbd34051
Mohayyad Muawia <mohayyad2.0@gmail.com>
2025-10-12 12:29:13 +0200

    Update user.ts

--- frontend/src/api/user.ts
+++ frontend/src/api/user.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_URL =
-  (import.meta.env.VITE_API_URL || "https://scholarmate-7vjy.onrender.com") +
+  (import.meta.env.VITE_API_URL || "http://localhost:3000") +
   "/user";
 const api = axios.create({ baseURL: API_URL });
 
